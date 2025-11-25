0a21bf20e3d21784dbe0bfc3ac7e4642c44c697d
hilt0n <ddjj676dd@gmail>
2025-11-25 18:46:04 +0300

    updated App.test

--- src/app/App.test.jsx
+++ src/app/App.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import App from "./App";
+import App from "../App.jsx";
 import { render, screen } from "@testing-library/react";
 
 describe("App component", () => {
